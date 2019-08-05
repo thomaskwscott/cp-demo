0378dcfa4583675dd3f8b915e954a89dd145d5fb
Tom Scott <tom@confluent.io>
2019-08-05 12:11:52 +0100

    including logbundler in demo

---
+++ scripts/logBundler/bin/log-bundler.sh
@@ -0,0 +1,18 @@
+# Which java to use
+if [ -z "$JAVA_HOME" ]; then
+  JAVA="java"
+else
+  JAVA="$JAVA_HOME/bin/java"
+fi
+
+# Memory options
+if [ -z "$KAFKA_HEAP_OPTS" ]; then
+  KAFKA_HEAP_OPTS="-Xmx256M"
+fi
+
+# JVM performance options
+if [ -z "$KAFKA_JVM_PERFORMANCE_OPTS" ]; then
+  KAFKA_JVM_PERFORMANCE_OPTS="-server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+DisableExplicitGC -Djava.awt.headless=true"
+fi
+
+exec $JAVA $KAFKA_HEAP_OPTS $KAFKA_JVM_PERFORMANCE_OPTS -cp ../libs/logBundler.jar io/confluent/logBundler/LogBundler "$@"
